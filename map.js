ef38fd1e8869677c485f0a5214b89769d3d6c6f8
Ignas <kridkid@gmail.com>
2016-11-10 23:45:28 +0200

    Generated map

--- map.js
+++ map.js
@@ -1,1 +1,29 @@
-console.log("it works");
+var height = 80;
+var length = 150;
+var map = [];
+
+for (var i = 0; i < height; i++) {
+    var lane = [];
+    for (var j = 0; j < length; j++) {
+        var cell = {
+            x: j,
+            y: i
+        };
+        lane.push(cell);
+    }
+    map.push(lane);
+}
+
+$('#map').css('width', 10 * length);
+
+$.each(map, function (index, lane) {
+    $.each(lane, function (index, cell) {
+        if (index == 0 || index == 1) {
+            $('#map').append('<div class="start"></div>');
+        } else if (index == length - 1 || index == length -2) {
+            $('#map').append('<div class="death"></div>');
+        } else {
+            $('#map').append('<div class="arena"></div>');
+        }
+    });
+});
