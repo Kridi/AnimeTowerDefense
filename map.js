8b1ce486cbf8caf38ab8cd0797b8d2b2f64bb49a
Ignas <kridkid@gmail.com>
2016-11-15 22:09:59 +0200

    Magic tower added

--- map.js
+++ map.js
@@ -18,12 +18,65 @@ $('#map').css('width', 10 * length);
 
 $.each(map, function (index, lane) {
     $.each(lane, function (index, cell) {
+        var cellElement;
         if (index == 0 || index == 1) {
-            $('#map').append('<div class="start"></div>');
+            cellElement = $('<div class="start"></div>');
         } else if (index == length - 1 || index == length -2) {
-            $('#map').append('<div class="death"></div>');
+            cellElement = $('<div class="death"></div>');
         } else {
-            $('#map').append('<div class="arena"></div>');
+            cellElement = $('<div class="arena"></div>');
         }
+
+        cellElement.data('x', cell.x);
+        cellElement.data('y', cell.y);
+
+        $('#map').append(cellElement);
+        cell.el = cellElement;
+        cell.neighbors = addNeighbors(cell);
     });
 });
+
+function addNeighbors(cell) {
+    var neighbors = [];
+    // nuo cell.x-2 iki cell.x+2
+    for (var i = getMinimumX(cell) ; i <= getMaximumX(cell); i++) {
+        for (var j = getMinimumY(cell) ; j <= getMaximumY(cell) ; j++) {
+            neighbors.push(map[j][i]);
+        }
+    }
+    return neighbors;
+}
+function getMinimumX(cell) {
+    var min = cell.x - 2;
+    if (min < 2) {
+        min = 2;
+    }
+    return min;
+}
+function getMaximumX(cell) {
+    var max = cell.x + 2;
+    if (max > length -3) {
+        max = length - 7;
+    }
+    if (getMinimumX(cell) == 2) {
+        max = 6;
+    }
+    return max;
+}
+function getMinimumY(cell) {
+    var min = cell.y - 2;
+    if (min < 0) {
+        min = 5;
+    }
+    if (getMaximumY(cell) == height -1) {
+        min = height - 5;
+    }
+    return min;
+}
+function getMaximumY(cell) {
+    var max = cell.y + 2;
+    if (max > height - 1) {
+        max = height - 1;
+    }
+    return max;
+}
---
+++ towers.js
@@ -0,0 +1,42 @@
+﻿var lastDragOver;
+var magicTower = '<img class="magic-tower-child" src="img/magic-tower.png"/>';
+
+document.addEventListener("dragstart", function (event) {
+    console.log("pavyko");
+});
+
+$('.arena').on('dragover', function(event) {
+    var x = $(event.target).data('x');
+    var y = $(event.target).data('y');
+    var cell = map[y][x];
+    for (var i = 0; i < cell.neighbors.length; i++) {
+        cell.neighbors[i].el.css('background-color', 'yellow');
+    }
+    lastDragOver = cell;
+});
+
+$('.arena').on('dragleave', function (event) {
+    var x = $(event.target).data('x');
+    var y = $(event.target).data('y');
+    var cell = map[y][x];
+    for (var i = 0; i < cell.neighbors.length; i++) {
+        cell.neighbors[i].el.css('background-color', '#cccccc');
+    }
+});
+
+$('#magic-tower').on('dragend', function (event) {
+    var x = lastDragOver.el.data('x');
+    var y = lastDragOver.el.data('y');
+    var cell = map[y][x];
+    for (var i = 0; i < cell.neighbors.length; i++) {
+        cell.neighbors[i].el.css('background-color', 'yellow');
+    }
+
+    console.log(lastDragOver.y + " " + lastDragOver.x);
+    var position = lastDragOver.neighbors[0].el.position();
+
+    var tower = $(magicTower);
+    tower.css({ top: position.top, left: position.left });
+
+    $('body').append(tower);
+});
