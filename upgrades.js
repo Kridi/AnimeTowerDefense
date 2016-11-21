b5904367bd161b4d2deb5de7a8f705a2ad8020e6
Ignas <kridkid@gmail.com>
2016-11-21 22:47:34 +0200

    Added stats

--- upgrades.js
+++ upgrades.js
@@ -16,6 +16,9 @@ $('#defence').on('click', function (event) {
     $('#panel-people').hide();
     console.log("defence has been clicked");
 });
-$('#fork').on('click', function(event) {
-    console.log("fork has been bought");
+$('#fork').on('click', function (event) {
+    var power = parseInt($('#power').html());
+    $('#power').html(power + 1);
+    console.log(power);
 });
+
