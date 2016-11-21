586e0aaf875f71bac7b64d71f3f0947275f5682f
Ignas <kridkid@gmail.com>
2016-11-21 22:00:33 +0200

    New better game

---
+++ upgrades.js
@@ -0,0 +1,21 @@
+﻿$('#weapons').on('click', function (event) {
+    $('#panel-weapons').toggle();
+    $('#panel-people').hide();
+    $('#panel-defence').hide();
+    console.log("weapons has been clicked");
+});
+$('#people').on('click', function (event) {
+    $('#panel-people').toggle();
+    $('#panel-defence').hide();
+    $('#panel-weapons').hide();
+    console.log("people has been clicked");
+});
+$('#defence').on('click', function (event) {
+    $('#panel-defence').toggle();
+    $('#panel-weapons').hide();
+    $('#panel-people').hide();
+    console.log("defence has been clicked");
+});
+$('#fork').on('click', function(event) {
+    console.log("fork has been bought");
+});
