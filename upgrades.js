132dd9b6de7a067796bff1d4452f453b3f830236
kridkid@gmail.com <kridkid@gmail.com>
2016-11-24 20:59:11 +0200

    Different prices for purchases

--- upgrades.js
+++ upgrades.js
@@ -21,4 +21,44 @@ $('#fork').on('click', function (event) {
     $('#power').html(power + 1);
     console.log(power);
 });
+$('#handgun').on('click', function (event) {
+    var power = parseInt($('#power').html());
+    $('#power').html(power + 5);
+    console.log(power);
+});
+$('#shotgun').on('click', function (event) {
+    var power = parseInt($('#power').html());
+    $('#power').html(power + 10);
+    console.log(power);
+});
+$('#pleb').on('click', function (event) {
+    var hp = parseInt($('#hp').html());
+    $('#hp').html(hp + 1);
+    console.log(hp);
+});
+$('#fighter').on('click', function (event) {
+    var hp = parseInt($('#hp').html());
+    $('#hp').html(hp + 5);
+    console.log(hp);
+});
+$('#swat').on('click', function (event) {
+    var hp = parseInt($('#hp').html());
+    $('#hp').html(hp + 10);
+    console.log(hp);
+});
+$('#tower').on('click', function (event) {
+    var def = parseInt($('#def').html());
+    $('#def').html(def + 1);
+    console.log(def);
+});
+$('#tower2').on('click', function (event) {
+    var def = parseInt($('#def').html());
+    $('#def').html(def + 5);
+    console.log(def);
+});
+$('#tower3').on('click', function (event) {
+    var def = parseInt($('#def').html());
+    $('#def').html(def + 10);
+    console.log(def);
+});
 
