f075c035c051fc401ad8455449565924d0c2aa90
Ignas <kridkid@gmail.com>
2016-11-25 16:59:55 +0200

    Different amount of stats on click for each upgrade.

--- upgrades.js
+++ upgrades.js
@@ -21,4 +21,45 @@ $('#axe').on('click', function (event) {
     $('#power').html(power + 1);
     console.log(power);
 });
+$('#chain').on('click', function (event) {
+    var power = parseInt($('#power').html());
+    $('#power').html(power + 3);
+    console.log(power);
+});
+$('#fork').on('click', function (event) {
+    var power = parseInt($('#power').html());
+    $('#power').html(power + 5);
+    console.log(power);
+});
+$('#boy').on('click', function (event) {
+    var lives = parseInt($('#hp').html());
+    $('#hp').html(lives + 1);
+    console.log(lives);
+});
+$('#girl').on('click', function (event) {
+    var lives = parseInt($('#hp').html());
+    $('#hp').html(lives + 3);
+    console.log(lives);
+});
+$('#peasant').on('click', function (event) {
+    var lives = parseInt($('#hp').html());
+    $('#hp').html(lives + 5);
+    console.log(lives);
+});
+$('#tower').on('click', function (event) {
+    var defence = parseInt($('#defences').html());
+    $('#defences').html(defence + 3);
+    console.log(defence);
+});
+$('#tower2').on('click', function (event) {
+    var defence = parseInt($('#defences').html());
+    $('#defences').html(defence + 5);
+    console.log(defence);
+});
+$('#tower3').on('click', function (event) {
+    var defence = parseInt($('#defences').html());
+    $('#defences').html(defence + 10);
+    console.log(defence);
+});
+
 
