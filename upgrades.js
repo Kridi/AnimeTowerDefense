fdda8c7b3c6fec0717cb4871251e242a84ed2209
0nida <piekute.rimante@gmail.com>
2016-12-01 18:32:22 +0200

    3rd tower random appearance and damage generated.

--- upgrades.js
+++ upgrades.js
@@ -83,6 +83,16 @@ $('#tower3').on('click', function (event) {
     if (game.resources.takeCoins(15)) {
         game.resources.addDefence(5);
     }
+    var tower = '<img src="img/defensive_sniper_tower.png" class="towers">';
+    var towerObject = $(tower);
+    $(towerObject).css({ top: randomNumberGenerators(635), left: 600 + randomNumberGenerators(380) });
+    $('#human').append(towerObject);
+
+    window.setInterval(function () {
+        var damage = 5;
+        var zombie = $($('.zombie')[randomNumberGenerators($('.zombie').length)]);
+        attackZombie(zombie, damage);
+    }, 1000);
 });
 
 
